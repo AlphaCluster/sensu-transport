fe360ee6fd7a1b45bd4c895ef3cf008ec5dcd24d
Nicholas Omann <AlphaCluster@linux.com>
2015-05-13 20:37:42 -0500

    Setup the new files that will be needed.
    Hopefully the qpid client wont be to hard to use with the base class.

---
+++ lib/sensu/transport/amqp.rb
@@ -0,0 +1,169 @@
+gem "qpid_messaging"
+
+require "qpid_messaging"
+
+require File.join(File.dirname(__FILE__), "base")
+
+module Sensu
+  module Transport
+    class AMQP < Base
+      def connect(options={})
+        reset
+        set_connection_options(options)
+        create_connection_timeout
+        connect_with_eligible_options
+      end
+
+      def reconnect
+        unless @reconnecting
+          @reconnecting = true
+          @before_reconnect.call
+          reset
+          periodically_reconnect
+        end
+      end
+
+      def connected?
+        @connection.connected?
+      end
+
+      def close
+        callback = Proc.new { @connection.close }
+        connected? ? callback.call : EM.next_tick(callback)
+      end
+
+      def publish(exchange_type, exchange_name, message, options={}, &callback)
+        begin
+          @channel.method(exchange_type.to_sym).call(exchange_name, options).publish(message) do
+            info = {}
+            callback.call(info) if callback
+          end
+        rescue => error
+          info = {:error => error}
+          callback.call(info) if callback
+        end
+      end
+
+      def subscribe(exchange_type, exchange_name, queue_name="", options={}, &callback)
+        previously_declared = @queues.has_key?(queue_name)
+        @queues[queue_name] ||= @channel.queue!(queue_name, :auto_delete => true)
+        queue = @queues[queue_name]
+        queue.bind(@channel.method(exchange_type.to_sym).call(exchange_name))
+        unless previously_declared
+          queue.subscribe(options, &callback)
+        end
+      end
+
+      def unsubscribe(&callback)
+        @queues.values.each do |queue|
+          if connected?
+            queue.unsubscribe
+          else
+            queue.before_recovery do
+              queue.unsubscribe
+            end
+          end
+        end
+        @queues = {}
+        @channel.recover if connected?
+        super
+      end
+
+      def acknowledge(info, &callback)
+        info.ack
+        callback.call(info) if callback
+      end
+
+      def stats(queue_name, options={}, &callback)
+        options = options.merge(:auto_delete => true)
+        @channel.queue(queue_name, options).status do |messages, consumers|
+          info = {
+            :messages => messages,
+            :consumers => consumers
+          }
+          callback.call(info)
+        end
+      end
+
+      private
+
+      def reset
+        @queues = {}
+        @connection_timeout.cancel if @connection_timeout
+        @connection.close_connection if @connection
+      end
+
+      def set_connection_options(options)
+        @connection_options = [options].flatten
+      end
+
+      def create_connection_timeout
+        @connection_timeout = EM::Timer.new(20) do
+          reconnect
+        end
+      end
+
+      def next_connection_options
+        if @eligible_options.nil? || @eligible_options.empty?
+          @eligible_options = @connection_options.shuffle
+        end
+        @eligible_options.shift
+      end
+
+      def reconnect_callback
+        Proc.new { reconnect }
+      end
+
+      def setup_connection(options={}, &callback)
+        @connection = AMQP.connect(options, {
+          :on_tcp_connection_failure => reconnect_callback,
+          :on_possible_authentication_failure => reconnect_callback
+        })
+        @connection.logger = @logger
+        @connection.on_open do
+          @connection_timeout.cancel
+          callback.call if callback
+        end
+        @connection.on_tcp_connection_loss(&reconnect_callback)
+        @connection.on_skipped_heartbeats(&reconnect_callback)
+      end
+
+      def setup_channel(options={})
+        @channel = AMQP::Channel.new(@connection)
+        @channel.auto_recovery = true
+        @channel.on_error do |channel, channel_close|
+          error = Error.new("rabbitmq channel closed")
+          @on_error.call(error)
+        end
+        prefetch = 1
+        if options.is_a?(Hash)
+          prefetch = options.fetch(:prefetch, 1)
+        end
+        @channel.prefetch(prefetch)
+      end
+
+      def connect_with_eligible_options(&callback)
+        options = next_connection_options
+        setup_connection(options, &callback)
+        setup_channel(options)
+      end
+
+      def periodically_reconnect
+        timer = EM::PeriodicTimer.new(5) do
+          unless connected?
+            begin
+              connect_with_eligible_options do
+                @reconnecting = false
+                @after_reconnect.call
+              end
+            rescue EventMachine::ConnectionError
+            rescue Java::JavaLang::RuntimeException
+            end
+          else
+            timer.cancel
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/amqp_spec.rb
@@ -0,0 +1,148 @@
+# Attempt to provide a real impl of AMQP not to be confused wtih RabbitMQ ;)
+
+require File.join(File.dirname(__FILE__), "helpers")
+require "sensu/transport/amqp"
+require "logger"
+
+describe "Sensu::Transport::AMQP" do
+  include Helpers
+
+  before do
+    @transport = Sensu::Transport::AMQP.new
+    @transport.logger = Logger.new(STDOUT)
+    @transport.logger.level = Logger::FATAL
+  end
+
+  it "provides a transport API" do
+    expect(@transport).to respond_to(:on_error, :before_reconnect, :after_reconnect,
+                                     :connect, :reconnect, :connected?, :close,
+                                     :publish, :subscribe, :unsubscribe,
+                                     :acknowledge, :ack, :stats)
+  end
+
+  it "can publish and subscribe" do
+    async_wrapper do
+      @transport.connect
+      callback = Proc.new do |message|
+        expect(message).to eq("msg")
+        timer(0.5) do
+          async_done
+        end
+      end
+      @transport.subscribe("direct", "foo", "baz", {}, &callback)
+      @transport.subscribe("direct", "bar", "baz", {}, &callback)
+      timer(1) do
+        @transport.publish("direct", "foo", "msg") do |info|
+          expect(info).to be_kind_of(Hash)
+          expect(info).to be_empty
+        end
+      end
+    end
+  end
+
+  it "can unsubscribe from queues and close the connection" do
+    async_wrapper do
+      @transport.connect
+      @transport.subscribe("direct", "bar") do |info, message|
+        true
+      end
+      timer(1) do
+        @transport.unsubscribe do
+          @transport.close
+          expect(@transport.connected?).to be(false)
+          async_done
+        end
+      end
+    end
+  end
+
+  it "can open and close the connection immediately" do
+    async_wrapper do
+      @transport.connect
+      @transport.close
+      expect(@transport.connected?).to be(false)
+      EM.next_tick do
+        expect(@transport.connected?).to be(false)
+        timer(1) do
+          expect(@transport.connected?).to be(false)
+          async_done
+        end
+      end
+    end
+  end
+
+  it "can acknowledge the delivery of a message" do
+    async_wrapper do
+      @transport.connect
+      @transport.subscribe("direct", "foo", "", :ack => true) do |info, message|
+        @transport.acknowledge(info) do
+          timer(0.5) do
+            async_done
+          end
+        end
+      end
+      timer(1) do
+        @transport.publish("direct", "foo", "msg") do |info|
+          expect(info).to be_kind_of(Hash)
+          expect(info).to be_empty
+        end
+      end
+    end
+  end
+
+  it "can get queue stats, message and consumer counts" do
+    async_wrapper do
+      @transport.connect
+      @transport.stats("bar") do |info|
+        expect(info).to be_kind_of(Hash)
+        expect(info[:messages]).to eq(0)
+        expect(info[:consumers]).to eq(0)
+        async_done
+      end
+    end
+  end
+
+  it "can fail to connect" do
+    async_wrapper do
+      @transport.connect(:port => 5555)
+      expect(@transport.connected?).to be(false)
+      async_done
+    end
+  end
+
+  it "will throw an error if it cannot resolve a hostname" do
+    async_wrapper do
+      expect {
+        @transport.connect(:host => "2def33c3-cfbb-4993-b5ee-08d47f6d8793")
+      }.to raise_error
+      async_done
+    end
+  end
+
+  it "can be configured for multiple brokers" do
+    async_wrapper do
+      @transport.connect([{:port => 5672}, {:port => 5672}])
+      timer(2) do
+        expect(@transport.connected?).to be(true)
+        async_done
+      end
+    end
+  end
+
+  it "can use TLS", :ssl => true do
+    ssl_dir = File.join(File.dirname(__FILE__), "assets", "ssl", "client")
+    async_wrapper do
+      @transport.connect(
+                         :port => 5671,
+                         :ssl => {
+                           :cert_chain_file => File.join(ssl_dir, "cert.pem"),
+                           :private_key_file => File.join(ssl_dir, "key.pem")
+                         }
+                         )
+      timer(2) do
+        expect(@transport.connected?).to be(true)
+        async_done
+      end
+    end
+  end
+end
